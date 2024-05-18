59e0e2a166a76e5f354b1fe42ecc6b8b8714d667
mamacraiscy <raiscyninamamac@gmail.com>
2024-05-18 21:17:18 +0800

    resolved al fixes needed in search

--- src/main/java/com/ctu/roommanagementportal/dbservices/CreateTables.java
+++ src/main/java/com/ctu/roommanagementportal/dbservices/CreateTables.java
@@ -12,50 +12,21 @@ import java.sql.Statement;
  */
 public class CreateTables {
 
-    private static final String createUserInfoTbl = "CREATE TABLE userinfo (" +
-            "firstName VARCHAR(255)," +
-            "middleName VARCHAR(255)," +
-            "lastName VARCHAR(255)," +
-            "birthDate DATE," +
-            "homeAddress VARCHAR(255)," +
-            "nationality VARCHAR(255)," +
-            "gender ENUM('Male', 'Female', 'Other')," +
-            "roleAtSchool VARCHAR(255)" +
-            ");";
-
-    private static final String createRoomRecordTbl = "CREATE TABLE RoomRecord (\n" +
-            "    roomNumber VARCHAR(255),\n" +
+    private static final String createRoomRecordTbl = "CREATE TABLE roomrecord (\n" +
+            "    roomName VARCHAR(255),\n" +
             "    capacity INT,\n" +
             "    roomStatus BOOLEAN,\n" +
             "    buildingLocation VARCHAR(255),\n" +
             "    maintenanceNotes TEXT,\n" +
             "    roomType VARCHAR(255),\n" +
-            "    hasProjector BOOLEAN\n" +
+            "    hasProjector BOOLEAN,\n" +
+            "    numOfChairs INT,\n" +
+            "    numOfComputers INT,\n" +
+            "    numOfDesks INT,\n" +
+            "    hasTV BOOLEAN,\n" +
+            "    hasInternetAccess BOOLEAN\n" +
             ");";
 
-    private static final String createBookingDetailsTbl = "CREATE TABLE BookingDetails (\n" +
-            "    userName VARCHAR(255),\n" +
-            "    email VARCHAR(255),\n" +
-            "    date DATE,\n" +
-            "    time TIME,\n" +
-            "    room VARCHAR(255),\n" +
-            "    bookingID INT\n" +
-            ");";
-
-    private static final String createAdminInfoTbl = "CREATE TABLE AdminInfo (\n" +
-            "    firstName VARCHAR(255),\n" +
-            "    middleName VARCHAR(255),\n" +
-            "    lastName VARCHAR(255),\n" +
-            "    birthDate DATE,\n" +
-            "    street VARCHAR(255),\n" +
-            "    barangay VARCHAR(255),\n" +
-            "    municipality VARCHAR(255),\n" +
-            "    city VARCHAR(255),\n" +
-            "    zipCode INT,\n" +
-            "    nationality VARCHAR(255),\n" +
-            "    gender VARCHAR(255),\n" +
-            "    roleAtSchool VARCHAR(255)\n" +
-            ");";
 
     public CreateTables(){
         try {
@@ -73,24 +44,17 @@ public class CreateTables {
         System.out.println("Establishing connection to DB");
 
         // Step 1: Establishing a Connection
-        try (Connection connection = DriverManager
-                .getConnection("jdbc:mysql://localhost:3306/roomportaldb?useSSL=false&allowPublicKeyRetrieval=true", "root", "admin123$");
+        try (Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/roomportaldb?useSSL=false&allowPublicKeyRetrieval=true",
+                "root",
+                "admin123$");
 
              // Step 2:Create a statement using connection object
              Statement statement = connection.createStatement();) {
 
             // Step 3: Execute the query or update query
-            System.out.println("Creating UserInfo Table in DB");
-            statement.execute(createUserInfoTbl);
-
             System.out.println("Creating RoomRecord Table in DB");
             statement.execute(createRoomRecordTbl);
-
-            System.out.println("Creating BookingDetails Table in DB");
-            statement.execute(createBookingDetailsTbl);
-
-            System.out.println("Creating AdminInfo Table in DB");
-            statement.execute(createAdminInfoTbl);
 
         } catch (SQLException e) {
 
--- src/main/java/com/ctu/roommanagementportal/infrastracture/CreateRoomRecord.java
+++ src/main/java/com/ctu/roommanagementportal/infrastracture/CreateRoomRecord.java
@@ -6,13 +6,9 @@
 package com.ctu.roommanagementportal.infrastracture;
 
 import com.ctu.roommanagementportal.abstraction.Room;
-import com.ctu.roommanagementportal.dbservices.CreateTables;
-import com.ctu.roommanagementportal.model.RoomType.Classroom;
-import com.ctu.roommanagementportal.model.RoomType.CompLaboratory;
-import com.ctu.roommanagementportal.model.RoomType.Library;
-import com.ctu.roommanagementportal.model.RoomType.Smartroom;
+import com.ctu.roommanagementportal.model.RoomType;
+import com.ctu.roommanagementportal.dbservices.InsertRecords;
 import java.sql.SQLException;
-
 import java.util.Scanner;
 
 
@@ -23,168 +19,124 @@ import java.util.Scanner;
  *
  */
 public class CreateRoomRecord {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
 //        CreateTables createTables = new CreateTables();
 
         Scanner scanner = new Scanner(System.in);
 
-        // Prompt use to select a room type
-        System.out.println("Select a room type:");
-        System.out.println("1. Classroom");
-        System.out.println("2. Computer Laboratory");
-        System.out.println("3. Library");
-        System.out.println("4. Smart room");
+        boolean roomInserted = false;
 
-        // Validate user input for room type
-        String roomType = InputValidator.validateRoomTypeInput(scanner);
+        while (!roomInserted) {
 
-        // Create a room record to the database
-        Room newRoom = createRoom(scanner, roomType); // Use createRoom to construct the room based on roomType
-        RoomDao roomDao = new RoomDao(); // Create a DAO instance
-        try {
-            roomDao.saveRoom(newRoom); // Save the room record to the database
-            System.out.println("Room record saved successfully.");
+            System.out.println("------------------");
+            System.out.println("Enter building location:");
+            System.out.println("------------------");
+            System.out.println("A - Admin Building");
+            System.out.println("T - College of Technology");
+            System.out.println("E - College of Engineering");
+            System.out.print("\nEnter your choice: ");
+            String buildingLocation = InputValidator.validateRoomBuilding(scanner);
 
-            // Display the details of what the user entered
-            System.out.println("Here are the details of the room you created:");
-            newRoom.displayRoomInfo(); // Display the user input
+            System.out.println("------------------");
+            System.out.println("Select a room type");
+            System.out.println("------------------");
+            System.out.println("1. Classroom");
+            System.out.println("2. Computer Laboratory");
+            System.out.println("3. Library");
+            System.out.println("4. Smart room");
+            System.out.print("\nEnter your choice: ");
 
-        } catch (SQLException e) {
-            System.err.println("Error saving room record: " + e.getMessage());
-//            e.printStackTrace();
+            String roomType = InputValidator.validateRoomTypeInput(scanner);
+
+            Room newRoom = createRoom(scanner, roomType, buildingLocation);
+
+            InsertRecords insertRecords = new InsertRecords();
+            boolean success = insertRecords.insertRoomRecord(newRoom);
+            if (success) {
+                System.out.println("Room record successfully inserted.");
+            } else {
+                System.out.println("A room with the same name and location already exists. Please try again with different details.");
+            }
+
+            System.out.print("\nDo you want to create another room record? (yes/no): ");
+            String createAnother = scanner.nextLine().trim().toLowerCase();
+
+            if (createAnother.equals("no")) {
+                roomInserted = true;
+            }
         }
 
-       scanner.close(); // Close scanner when finished
+        System.out.println("\nThank you for using the room record creation system. Goodbye!");
+        scanner.close();
     }
 
-    /**
-     * Create a room object based on user input
-     *
-     * @param scanner  The scanner object for user input.
-     * @param roomType The type of room selected by the user.
-     * @return The created room object
-     */
-    private static Room createRoom(Scanner scanner, String roomType) {
+    private static Room createRoom(Scanner scanner, String roomType, String buildingLocation) {
+        System.out.print("\nEnter room name: ");
+        String roomName = InputValidator.validateNonEmptyStringInput(scanner);
 
-        System.out.println("Enter room number: ");
-        String roomNumber = InputValidator.validateNonEmptyStringInput(scanner); // Ensures non-empty input
-
-        System.out.println("Enter room capacity: ");
+        System.out.print("\nEnter room capacity: ");
         int capacity = InputValidator.validateIntegerInput(scanner);
 
-        System.out.println("Is the room available?(yes/no): ");
+        System.out.print("\nEnter number of chairs: ");
+        int numOfChairs = InputValidator.validateIntegerInput(scanner);
+
+        System.out.print("\nIs the room available? (yes/no): ");
         boolean roomStatus = InputValidator.validateBooleanInput(scanner);
 
-        System.out.println("Does it have a projector?(yes/no): ");
-        boolean hasProjector =InputValidator.validateBooleanInput(scanner);
+        System.out.print("\nDoes it have a projector? (yes/no): ");
+        boolean hasProjector = InputValidator.validateBooleanInput(scanner);
 
-        System.out.println("Enter building location. Available Buildings are..");
-        System.out.println("'Admin' for Admin Building");
-        System.out.println("'COT' for College of Technology.");
-        System.out.println("'COE' for College of Engineering.");
-        String buildingLocation = InputValidator.validateRoomBuilding(scanner).trim();
-
-        System.out.println("Provide here the things that needs maintenance: ");
+        System.out.print("\nProvide maintenance notes: ");
         String maintenanceNotes = InputValidator.validateNonEmptyStringInput(scanner).trim();
+
 
         switch (roomType) {
             case "1":
-                return createClassroom(scanner, roomNumber, capacity, roomStatus, hasProjector, buildingLocation, maintenanceNotes);
+                return createClassroom(scanner, roomName, capacity, roomStatus, hasProjector, buildingLocation, maintenanceNotes, numOfChairs);
             case "2":
-                return createLaboratory(scanner, roomNumber, capacity, roomStatus, hasProjector, buildingLocation, maintenanceNotes);
+                return createLaboratory(scanner, roomName, capacity, roomStatus, hasProjector, buildingLocation, maintenanceNotes,numOfChairs);
             case "3":
-                return createLibrary(scanner, roomNumber, capacity, roomStatus, hasProjector, buildingLocation, maintenanceNotes);
+                return createLibrary(scanner, roomName, capacity, roomStatus, hasProjector, buildingLocation, maintenanceNotes, numOfChairs);
             case "4":
-                return createSmartroom(scanner, roomNumber, capacity, roomStatus, hasProjector, buildingLocation, maintenanceNotes);
+                return createSmartroom(scanner, roomName, capacity, roomStatus, hasProjector, buildingLocation, maintenanceNotes, numOfChairs);
             default:
                 throw new IllegalArgumentException("Invalid room type.");
         }
     }
 
-    /**
-     * Creates a classroom object based on user input.
-     *
-     * @param scanner          The scanner object for user input.
-     * @param roomNumber       The room number.
-     * @param capacity         The capacity of the room.
-     * @param roomStatus       The availability status of the room.
-     * @param hasProjector     Indicates whether the room has a projector.
-     * @param buildingLocation The location of the building.
-     * @param maintenanceNotes Maintenance notes for the room.
-     * @return The created classroom object.
-     */
-    private static Room createClassroom(Scanner scanner, String roomNumber, int capacity, boolean roomStatus, boolean hasProjector,
-                                        String buildingLocation, String maintenanceNotes) {
-        System.out.println("Enter number of chairs:");
-        int numOfChairs = InputValidator.validateIntegerInput(scanner); // Correct initialization
+    private static Room createClassroom(Scanner scanner, String roomName, int capacity, boolean roomStatus, boolean hasProjector,
+                                        String buildingLocation, String maintenanceNotes, int numOfChairs) {
+        System.out.print("\nDoes it have a whiteboard? (yes/no): ");
+        boolean hasWhiteboard = InputValidator.validateBooleanInput(scanner);
 
-        return new Classroom(roomNumber, capacity, roomStatus, buildingLocation, maintenanceNotes, hasProjector, numOfChairs); // Correct constructor call
+        return new RoomType.Classroom(roomName, capacity, roomStatus, buildingLocation, maintenanceNotes, hasProjector, numOfChairs,hasWhiteboard);
     }
 
-    /**
-     * Create as a computer laboratory object based on user input.
-     *
-     * @param scanner          The scanner object for user input.
-     * @param roomNumber       The room number.
-     * @param capacity         The capacity of the room.
-     * @param roomStatus       The availability status of the room.
-     * @param hasProjector     Indicates whether the room has a projector.
-     * @param buildingLocation The location of the building.
-     * @param maintenanceNotes Maintenance notes for the room.
-     * @return The created computer laboratory.
-     */
-    private static Room createLaboratory(Scanner scanner, String roomNumber, int capacity, boolean roomStatus, boolean hasProjector,
-                                         String buildingLocation, String maintenanceNotes) {
-        System.out.println("Enter number of computers:");
+    private static Room createLaboratory(Scanner scanner, String roomName, int capacity, boolean roomStatus, boolean hasProjector,
+                                         String buildingLocation, String maintenanceNotes, int numOfChairs) {
+        System.out.print("\nEnter number of computers: ");
         int numOfComputers = InputValidator.validateIntegerInput(scanner);
 
-        return new CompLaboratory(roomNumber, capacity, roomStatus, buildingLocation, maintenanceNotes, hasProjector, numOfComputers); // Correct constructor call
+        return new RoomType.CompLaboratory(roomName, capacity, roomStatus, buildingLocation, maintenanceNotes, hasProjector, numOfChairs, numOfComputers);
     }
 
-    /**
-     * Creates a library objects based on user input.
-     *
-     * @param scanner          The scanner object for user input.
-     * @param roomNumber       The room number.
-     * @param capacity         The capacity of the room.
-     * @param roomStatus       The availability status of the room.
-     * @param hasProjector     Indicates whether the room has a projector.
-     * @param buildingLocation The location of the building.
-     * @param maintenanceNotes Maintenance notes for the room.
-     * @return The created library object.
-     */
-    private static Room createLibrary(Scanner scanner, String roomNumber, int capacity, boolean roomStatus, boolean hasProjector,
-                                      String buildingLocation, String maintenanceNotes) {
-        System.out.println("Enter number of desks:");
-        int numOfDesks = InputValidator.validateIntegerInput(scanner); // Ensure correct initialization
+    private static Room createLibrary(Scanner scanner,String roomName, int capacity, boolean roomStatus, boolean hasProjector,
+                                      String buildingLocation, String maintenanceNotes, int numOfChairs) {
+        System.out.print("\nEnter number of desks: ");
+        int numOfDesks = InputValidator.validateIntegerInput(scanner);
 
-        return new Library(roomNumber, capacity, roomStatus, buildingLocation, maintenanceNotes, hasProjector, numOfDesks); // Correct constructor call
+        return new RoomType.Library(roomName, capacity, roomStatus, buildingLocation, maintenanceNotes, hasProjector, numOfChairs, numOfDesks);
     }
 
-    /**
-     * Creates a smart room object based on user input.
-     *
-     * @param scanner          The scanner object for user input.
-     * @param roomNumber       The room number.
-     * @param capacity         The capacity of the room.
-     * @param roomStatus       The availability status of the room.
-     * @param hasProjector     Indicates whether the room has a projector.
-     * @param buildingLocation The location of the building.
-     * @param maintenanceNotes Maintenance notes for the room.
-     * @return The created smart room object.
-     */
-    private static Room createSmartroom(Scanner scanner, String roomNumber, int capacity, boolean roomStatus, boolean hasProjector,
-                                        String buildingLocation, String maintenanceNotes) {
-        System.out.println("Does it have a TV? (yes/no):");
-        boolean hasTV = InputValidator.validateBooleanInput(scanner); // Correct variable initialization
+    private static Room createSmartroom(Scanner scanner, String roomName, int capacity, boolean roomStatus, boolean hasProjector,
+                                        String buildingLocation, String maintenanceNotes, int numOfChairs) {
+        System.out.print("\nDoes it have a TV? (yes/no): ");
+        boolean hasTV = InputValidator.validateBooleanInput(scanner);
 
-        System.out.println("Does it have internet connection? (yes/no):");
-        boolean hasInternetAccess = InputValidator.validateBooleanInput(scanner); // Correct variable initialization
+        System.out.print("\nDoes it have internet connection? (yes/no): ");
+        boolean hasInternetAccess = InputValidator.validateBooleanInput(scanner);
 
-        return new Smartroom(roomNumber, capacity, roomStatus, buildingLocation, maintenanceNotes, hasProjector, hasTV, hasInternetAccess); // Correct constructor call
+        return new RoomType.Smartroom(roomName, capacity, roomStatus, buildingLocation, maintenanceNotes, hasProjector,hasTV, hasInternetAccess, numOfChairs);
     }
-
 }
-
-
